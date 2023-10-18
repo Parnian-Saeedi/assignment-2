7f8238e561d73f25d811cc826788ecdf6ec5dced
Parnian-Saeedi <148317070+Parnian-Saeedi@users.noreply.github.com>
2023-10-18 18:18:23 +0900

    Add files via upload

---
+++ tamrin 3.py
@@ -0,0 +1,12 @@
+a = input("enter name & family :")
+x = int(input("enter grade 1: "))
+y = int(input("enter grade 2: "))
+z = int(input("enter grade 3: "))
+avg = (x+y+z)/3
+
+if avg > 17:
+    print("greatee")
+if avg <=17 and avg >=12:
+     print("normal")
+if  avg<12:
+    print("fail")
